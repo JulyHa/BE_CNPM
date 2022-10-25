061afe444be7fd0dc6caaf3dfae0cdfc986a5c51
JulyHa <nguyenthingu3107@gmail.com>
2022-10-25 23:04:08 +0700

    create project

---
+++ src/main/java/utc/edu/hongheo/HoNgheoApplication.java
@@ -0,0 +1,13 @@
+package utc.edu.hongheo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoNgheoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoNgheoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utc/edu/hongheo/HoNgheoApplicationTests.java
@@ -0,0 +1,13 @@
+package utc.edu.hongheo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoNgheoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
